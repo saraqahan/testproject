22ac6bbbc57aae901edb8b20d1d11f5e4cb40cca
saraqahan <saraqahan@gmail.com>
2019-08-21 22:19:12 +0300

    Daay2.py

---
+++ Daay2.py
@@ -0,0 +1,15 @@
+#This is a commment
+print("hello ,world")
+print("hello ,world") #This is a commment
+#print("Hello")
+print("Cheers , Mate !")
+#This is a commment
+#written in
+#more than just one line
+print ("Hello, world")
+"""
+This is comment
+written in
+more than just one line
+"""
+print ("Hello, world")
