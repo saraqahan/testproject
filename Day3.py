e609dac12341a2c1d18be2736fe8e03030101417
saraqahan <saraqahan@gmail.com>
2019-08-23 00:42:43 +0300

    Day3.py

---
+++ Day3.py
@@ -0,0 +1,30 @@
+x=5
+y="Jhon"
+print(x)
+print(y)
+x=4
+x="Sally"
+print(x)
+x=("Jhon")
+print(x)
+x='Jhon'
+print(x)
+x,y,z ="orange","Banana","Cherry"
+print(x)
+print(y)
+print(z)
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+x="awesome"
+print("python is " +x)
+x="python is"
+y="awesome"
+z=x+y
+x=5
+y=10
+print(x+y)
+x=5
+y="John"
+print(x+y)
