d4a3b9e29fffee3be8c1d188ab80ffedd4629909
saraqahan <saraqahan@gmail.com>
2019-09-08 22:34:15 +0300

    Day8.py

---
+++ Day8.py
@@ -0,0 +1,12 @@
+a = "Hello ,World!"
+print(a.strip())
+a = "Hello ,World!"
+print(len(a))
+a="Hello,world"
+print(a.lower())
+a="Hello,world"
+print(a.upper())
+a="Hello,world"
+print(a.replace("H","J"))
+a="Hello,world"
+print(a.split(","))
