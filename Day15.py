af0896502ab1078746e9ba8b0608920fd36c6a83
saraqahan <saraqahan@gmail.com>
2019-09-10 15:23:27 +0300

    Day15.py

---
+++ Day15.py
@@ -0,0 +1,25 @@
+thislist=["apple","banana","cherry"]
+print(len(thislist))
+thislist=["apple","banana","cherry"]
+thislist.append("orange")
+print(thislist)
+thislist=["apple","banana","cherry"]
+thislist.insert(1,"orange")
+print(thislist)
+thislist=["apple","banana","cherry"]
+thislist.remove("banana")
+print(thislist)
+thislist=["apple","banana","cherry"]
+thislist.pop()
+print(thislist)
+thislist=["apple","banana","cherry"]
+thislist.clear()
+print(thislist)
+thislist=["apple","banana","cherry"]
+mylist=thislist.copy()
+print(mylist)
+thislist=["apple","banana","cherry"]
+mylist= list(thislist)
+print(mylist)
+thislist = list(("apple","banana","cherry"))
+print(thislist)
