f4f7f9ee5fc99ffce11ea5bf3eac88a5faae81d3
saraqahan <saraqahan@gmail.com>
2019-09-10 13:59:37 +0300

    Day13.py

---
+++ Day13.py
@@ -0,0 +1,22 @@
+s=[]
+print(s)
+numbers = [1,2,3,4,5]
+print(numbers)
+thislist =["apple","banana","cherry"]
+print(thislist)
+thislist =["apple","banana","cherry",1,2,3]
+print(thislist)
+thislist =["apple","banana","cherry"]
+print(thislist[1])
+thislist =["apple","banana","cherry"]
+for x in thislist:
+  print(x)
+  thislist =["apple","banana","cherry"]
+  thislist[1]= "blackcurrant"
+  print(thislist)
+  thislist =["apple","banana","cherry"]
+  del thislist[0]
+  print(thislist)
+  thislist=["apple","banana","cherry"]
+  del thislist
+  
