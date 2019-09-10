3c80931e6a7445b7629996c92c72ada3aa2a8c6e
saraqahan <saraqahan@gmail.com>
2019-09-10 14:19:08 +0300

    Day14.py

---
+++ day14.py
@@ -0,0 +1,9 @@
+thislist=["apple","banana","cherry"]
+if "apple" in thislist:
+    print("yas,is in the fruits list")
+thislist =["python"]*4
+print(thislist)
+thislist1=["Ahmad","khalid","omar"]
+thislist2=["sara","Hind","Batool"]
+thislist3=thislist1+thislist2
+print(thislist3)
