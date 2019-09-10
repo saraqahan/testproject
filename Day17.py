f83fdfb66e27e3efec8ef2882ceab4c7e771c03e
saraqahan <saraqahan@gmail.com>
2019-09-10 21:13:32 +0300

    Day17.py

---
+++ Day17.py
@@ -0,0 +1,23 @@
+thistuple=("apple","banana","cherry")
+print(thistuple)
+thistuple =()
+print(thistuple)
+thistuple =(3,)
+print(thistuple)
+thistuple =(3,1.3,4.1,7)
+print(thistuple)
+thistuple=("Ahmad",1.1,4,"بايثون")
+print(thistuple)
+thistuple =("apple","banana","cherry")
+print(thistuple[1])
+thistuple =("apple","banana","cherry")
+for x in thistuple:
+    print(x)
+#thistuple =("apple","banana","cherry")
+#thistuple[3]="Orange"
+#print(thistuple)
+#thistupe =("apple","banana","cherry")
+#del thistuple
+#print(thistupe)
+thistuple = ("apple","banana","cherry","orange")
+print(thistuple [0:3])
