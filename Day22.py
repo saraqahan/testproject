5d55a4d15255e615cce9492c268f79dbc6f467bf
saraqahan <saraqahan@gmail.com>
2019-09-12 00:25:26 +0300

    Day22.py

---
+++ Day22.py
@@ -0,0 +1,61 @@
+thisdice = {}
+print(thisdice)
+thisdice ={
+"brand" : "Ford",
+"model" : "Mustang",
+"year" :1964
+}
+print(thisdice)
+thisdice ={
+"brand" : "Ford",
+"model" : "Mustang",
+"year" :1964
+}
+x = thisdice["model"]
+print(x)
+thisdice ={
+"brand" : "Ford",
+"model" : "Mustang",
+"year" :1964
+}
+x = thisdice.get("model")
+print(x)
+thisdice ={
+"brand" : "Ford",
+"model" : "Mustang",
+"year" :1964
+}
+thisdice["year"]=2018
+print(thisdice)
+thisdice ={
+"brand" : "Ford",
+"model" : "Mustang",
+"year" :1964
+}
+for x in thisdice:
+    print(x)
+thisdice = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+    }
+for x in thisdice:
+        print(thisdice[x])
+thisdice ={
+"brand" : "Ford",
+"model" : "Mustang",
+"year" :1964
+}
+for x in thisdice.values():
+    print(x)
+thisdict= {"brand":"Ford","model":"Mustang","year":1964}
+print(thisdict.values())
+thisdice ={
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" :1964
+    }
+for x,y in thisdice.items():
+        print(x,y)
+thisdict = {"brand":"Ford","model":"Mustang","year":1964}
+print(thisdict.items())
