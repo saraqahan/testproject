93246f09c9595dfe92e53b2df9eb00ea29a23458
saraqahan <saraqahan@gmail.com>
2019-09-11 23:01:18 +0300

    Day20.py

---
+++ Day20.py
@@ -0,0 +1,17 @@
+thisset ={}
+print(thisset)
+thisset ={"apple","banana","cheryy"}
+print(thisset)
+thisset={"Ahmad","Ahmad",1,2,1,5}
+print(thisset)
+thisset ={"apple","banana","cheryy"}
+for x in thisset:
+  print(x)
+thisset ={"apple","banana","cheryy"}
+print("banana" in thisset)
+thisset ={"apple","banana","cheryy"}
+thisset.add("orange")
+print(thisset)
+thisset ={"apple","banana","cheryy"}
+thisset.update(["orange","mango","grapes"])
+print(thisset)
