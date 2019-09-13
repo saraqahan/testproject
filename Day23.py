e871553cc510ec0591d42d2ffe7386e4ca85143a
saraqahan <saraqahan@gmail.com>
2019-09-13 23:40:43 +0300

    Day23.py

---
+++ Day23.py
@@ -0,0 +1,48 @@
+thisdict={
+"brand": "Ford",
+"model":"Mustang",
+"year":1964
+}
+if "model" in thisdict:
+    print("Yes,'model'isone of the keys in thisdict dictionary")
+thisdict={
+"brand": "Ford",
+"model":"Mustang",
+"year":1964
+}
+print(len(thisdict))
+thisdict={
+"brand": "Ford",
+"model":"Mustang",
+"year":1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+thisdict={
+"brand": "Ford",
+"model":"Mustang",
+"year":1964
+}
+thisdict.pup("model")
+print(thisdict)
+thisdict={
+"brand": "Ford",
+"model":"Mustang",
+"year":1964
+}
+thisdict.pupitem()
+print(thisdict)
+thisdict={
+"brand": "Ford",
+"model":"Mustang",
+"year":1964
+}
+del thisdict["model"]
+print(thisdict)
+thisdict={
+"brand": "Ford",
+"model":"Mustang",
+"year":1964
+}
+thisdict.clear()
+print(thisdict)
