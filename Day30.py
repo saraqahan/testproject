0faaa3883ecd7585ad90353b122b888169d378fe
saraqahan <saraqahan@gmail.com>
2019-09-25 22:54:24 +0300

    Day30.py

---
+++ Day30.py
@@ -0,0 +1,15 @@
+for x in range(6):
+    print(x)
+for x in range(2,6):
+    print(x)
+for x in range(2,30,3):
+    print(x)
+for x in range(6):
+    print(x)
+else:
+    print("Finally frinished!")
+adj = ["red","big","tasty"]
+fruits =["apple","banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
