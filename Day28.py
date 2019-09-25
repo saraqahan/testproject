382f00e69088a192d9bcedda199a36d27c0d3a90
saraqahan <saraqahan@gmail.com>
2019-09-25 21:53:37 +0300

    Day28.py

---
+++ Day28.py
@@ -0,0 +1,24 @@
+i=1
+while i<6:
+    print(i)
+    i+=1
+i=1
+while i<6:
+    print(i)
+    i+=1
+    if i==3:
+        print(i)
+        break
+i=0
+while i<6:
+    i +=1
+    print(i)
+    if i==3:
+        continue
+        print(i)
+i=1
+while i<6:
+    print(i)
+    i +=1
+else:
+    print("i is no longer less thin 6")
