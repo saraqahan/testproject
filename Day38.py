9f282f7538114740213a21c8e8d6e7257f8908e5
saraqahan <saraqahan@gmail.com>
2019-09-27 14:49:14 +0300

    Day38.py

---
+++ Day38.py
@@ -0,0 +1,12 @@
+cars=["ford","Volvo","BMW"]
+for x in cars:
+    print(x)
+cars=["ford","Volvo","BMW"]
+cars.append("Honda")
+print(cars)
+cars=["ford","Volvo","BMW"]
+cars.pop(1)
+print(cars)
+cars=["ford","Volvo","BMW"]
+cars.remove("Volvo")
+print(cars)
